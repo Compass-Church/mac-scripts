7d8779053d0b8c77467d4def9d34e8dbfa536a8d
Preston Mainard <62040408+pmainard@users.noreply.github.com>
2020-07-23 17:18:59 -0500

    Add files via upload

---
+++ map-uniflow.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+## Jamf Policy Parameters
+jssUser=$4
+jssPass=$5
+jssHost=$6
+uniFLOWurl=$7
+
+## Load Mac Serial Number
+serial=$(ioreg -rd1 -c IOPlatformExpertDevice | awk -F'"' '/IOPlatformSerialNumber/{print $4}')
+
+## Use Jamf Pro API to get assigned username
+username=$(/usr/bin/curl -H "Accept: text/xml" -sfku "${jssUser}:${jssPass}" "${jssHost}/JSSResource/computers/serialnumber/${serial}/subset/location" | xmllint --format - 2>/dev/null | awk -F'>|<' '/<username>/{print $3}')
+
+## If either the username came back blank from the above API calls, exit
+if [[ "$username" == "" ]]; then
+    echo "Error - Username blank"
+    exit 1
+## Map UniFlow Secure and Delegated queues
+else
+    /usr/sbin/lpadmin -p "SecurePrint" -E -v lpd://$userName@$uniFLOWurl/CanonSecurePrint -P /etc/cups/ppd/SecurePrint.ppd -D "SecurePrint"
+    /usr/sbin/lpadmin -p "DelegatedPrint" -E -v lpd://$userName@$uniFLOWurl/DelegatedSecureprint -P /etc/cups/ppd/DelegatedPrint.ppd -D "DelegatedPrint"  
+fi
